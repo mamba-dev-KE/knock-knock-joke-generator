2f0564cfe40bc87e3a986ee51a1041ce0a872da5
mamba-dev-KE <josephmaramba50@gmail.com>
2021-04-15 16:08:48 +0300

    File renaming to suite message generator code

---
+++ knockKnockJokeGenerator.js
@@ -0,0 +1,29 @@
+let number = 0;
+const jokeGenerator = number => {
+    number = Math.floor(Math.random() * 10);
+    switch (randomNumber) {
+      case 0:
+        return 'happy';
+      case 1:
+        return 'sad';
+      case 2:
+        return 'satisfied';
+      case 3:
+        return 'calm';
+      case 4:
+        return 'calm';
+      case 5:
+        return 'calm';
+      case 6:
+        return 'calm';
+      case 7:
+        return 'calm';
+      case 8:
+        return 'calm';
+      case 9:
+        return 'calm';
+
+    }
+  };
+
+console.log(messageGenerator(randomNumber));
